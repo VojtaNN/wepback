ef861df9b7fdc24342545b220ae2d11f1a72e8d4
VojtaNN <61760213+VojtaNN@users.noreply.github.com>
2021-09-24 21:21:01 +0200

    0.1.0
    + Added configuration of commands
    + Added configuration for changing weapon on back or on chest

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {}
+ 
+ Config.Connect = "buckle"  ------- Restart the server for the changes to take effect
+
+ Config.Release = "unbuckle"   ------- Restart the server for the changes to take effect
+ 
+ Config.positionchest = false
+ 
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'VojtaNN'
+--description 'https://github.com/overextended/ox_inventory'
+--versioncheck 'https://raw.githubusercontent.com/overextended/ox_inventory/main/fxmanifest.lua'
+version '0.1.0'
+
+client_scripts { 
+'config.lua',
+'client.lua'
+}
